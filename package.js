dbd33df2d972b04856b06dfaaa9e567c68e1b321
Eric Dobbertin <eric@dairystatedesigns.com>
2015-03-09 11:55:54 -0500

    support af5

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: 'aldeed:autoform-bs-datepicker',
   summary: 'Custom bootstrap-datepicker input type for AutoForm',
-  version: '1.1.0',
+  version: '1.1.1',
   git: 'https://github.com/aldeed/meteor-autoform-bs-datepicker.git'
 });
 
 Package.onUse(function(api) {
   api.use('templating@1.0.0');
   api.use('blaze@2.0.0');
-  api.use('aldeed:autoform@4.0.0');
+  api.use('aldeed:autoform@4.0.0 || 5.0.0');
   api.addFiles([
     'autoform-bs-datepicker.html',
     'autoform-bs-datepicker.js'
