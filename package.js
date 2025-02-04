923fbbf6e5ede09517282875cad909ac57e8104f
Jan Dvorak <storytellercz@proton.me>
2025-02-04 01:56:44 +0100

    Up minimum Meteor version due to autoform v6

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'aldeed:autoform-bs-datepicker',
   summary: 'Custom bootstrap-datepicker input type for AutoForm',
-  version: '2.0.1',
+  version: '2.0.2',
   git: 'https://github.com/aldeed/meteor-autoform-bs-datepicker.git'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom(['1.12.1', '2.16']);
+  api.versionsFrom('2.16');
   api.use('templating@1.0.0 || 1.4.4');
   api.use('blaze@2.0.0 || 2.9.0 || 3.0.1');
   api.use('underscore');
