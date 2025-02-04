0eab9bf179deecfbcdef057e68e3797e180950ce
Jan Dvorak <hello@jandvorak.me>
2025-02-04 01:45:35 +0100

    Support Meteor 1.12.1 and 2.16
    Update the package.js to better support Meteor 1.12.1 and 2.16

--- package.js
+++ package.js
@@ -1,14 +1,15 @@
 Package.describe({
   name: 'aldeed:autoform-bs-datepicker',
   summary: 'Custom bootstrap-datepicker input type for AutoForm',
-  version: '2.0.0',
+  version: '2.0.1',
   git: 'https://github.com/aldeed/meteor-autoform-bs-datepicker.git'
 });
 
 Package.onUse(function(api) {
-  api.use('templating@1.0.0');
-  api.use('blaze@2.0.0');
-  api.use('underscore@1.0.0');
+  api.versionsFrom(['1.12.1', '2.16']);
+  api.use('templating');
+  api.use('blaze');
+  api.use('underscore');
   api.use('aldeed:autoform@6.0.0');
   api.addFiles([
     'autoform-bs-datepicker.html',
